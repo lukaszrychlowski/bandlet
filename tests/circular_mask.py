e9d73608b6ea915795f10c6cb9b85643d1e46d68
lukaszrychlowski <rychlowski@agh.edu.pl>
2021-10-18 22:41:53 +0200

    shift is neglible for bigger img sizes

--- tests/circular_mask.py
+++ tests/circular_mask.py
@@ -1,12 +1,16 @@
 import numpy as np
+from PIL import Image
 
 def circular_mask(img_width, img_height, mask_radius):
     x, y = np.ogrid[:img_width, :img_height]
-    distance_field = np.sqrt((x - round(0.5*img_width, ndigits=None))**2 + (y-round(0.5*img_height, ndigits=None))**2)
+    distance_field = np.sqrt((x - round(0.5*img_width, ndigits=None))**2 + 
+                             (y - round(0.5*img_height, ndigits=None))**2)
     print(distance_field)
     circular_mask = distance_field <= mask_radius
     return circular_mask
 
-img_width, img_height, radius = 8,8,2
+img_width, img_height, radius = 150,150,70
 mask = circular_mask(img_width, img_height, radius)
-print(mask)
+
+im = Image.fromarray(mask)
+im.show()
