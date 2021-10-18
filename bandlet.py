77f1092381db9663c1606cf78eca5c2ba9e64e53
lukaszrychlowski <rychlowski@agh.edu.pl>
2021-10-18 22:17:10 +0200

    distance_field nan values fixed

--- bandlet.py
+++ bandlet.py
@@ -16,7 +16,7 @@ ft_img = np.log10(np.abs(ft))
 
 def circular_mask(img_width, img_height, mask_radius):
     x, y = np.ogrid[:img_width, :img_height]
-    distance_field = np.sqrt((x - round(0.5*img_width)**2 + (y - round(0.5*img_height)**2)))
+    distance_field = np.sqrt((x - round(0.5*img_width, ndigits=None))**2 + (y-round(0.5*img_height, ndigits=None))**2)
     print(distance_field)
     circular_mask = distance_field <= mask_radius
     return circular_mask
