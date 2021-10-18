1381f8e589477b9f21a4849b983970b8e165529b
lukaszrychlowski <rychlowski@agh.edu.pl>
2021-10-18 22:16:38 +0200

    nan values fixed

--- bandlet.py
+++ bandlet.py
@@ -14,18 +14,13 @@ ft = np.fft.fft2(img)
 ft = np.fft.fftshift(ft)
 ft_img = np.log10(np.abs(ft))
 
-#plt.imshow(ft_img)
-#plt.imsave('/Users/ryszard/Desktop/'+'img.png', img)
-#plt.show()
-
 def circular_mask(img_width, img_height, mask_radius):
     x, y = np.ogrid[:img_width, :img_height]
-   
-    distance = np.sqrt((x - round(0.5*img_width, ndigits=None))**2 + (y-round(0.5*img_height, ndigits=None))**2)
-    print(distance)
-    circular_mask = distance <= mask_radius
+    distance_field = np.sqrt((x - round(0.5*img_width)**2 + (y - round(0.5*img_height)**2)))
+    print(distance_field)
+    circular_mask = distance_field <= mask_radius
     return circular_mask
 
-img_width, img_height = 6,6
-mask = circular_mask(img_width, img_height, 1)
+img_width, img_height, radius = 8,8,2
+mask = circular_mask(img_width, img_height, radius)
 print(mask)
--- tests/circular_mask.py
+++ tests/circular_mask.py
@@ -2,12 +2,11 @@ import numpy as np
 
 def circular_mask(img_width, img_height, mask_radius):
     x, y = np.ogrid[:img_width, :img_height]
-   
-    distance = np.sqrt((x - round(0.5*img_width, ndigits=None))**2 + (y-round(0.5*img_height, ndigits=None))**2)
-    print(distance)
-    circular_mask = distance <= mask_radius
+    distance_field = np.sqrt((x - round(0.5*img_width, ndigits=None))**2 + (y-round(0.5*img_height, ndigits=None))**2)
+    print(distance_field)
+    circular_mask = distance_field <= mask_radius
     return circular_mask
 
-img_width, img_height, radius = 5,5,1
+img_width, img_height, radius = 8,8,2
 mask = circular_mask(img_width, img_height, radius)
 print(mask)
