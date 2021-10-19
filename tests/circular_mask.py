04267a783438c02b32361799c5e7799cac6eaa25
lukaszrychlowski <rychlowski@agh.edu.pl>
2021-10-19 07:59:11 +0200

    cleaned a bit

--- tests/circular_mask.py
+++ tests/circular_mask.py
@@ -5,11 +5,10 @@ def circular_mask(img_width, img_height, mask_radius):
     x, y = np.ogrid[:img_width, :img_height]
     distance_field = np.sqrt((x - round(0.5*img_width, ndigits=None))**2 + 
                              (y - round(0.5*img_height, ndigits=None))**2)
-    print(distance_field)
     circular_mask = distance_field <= mask_radius
     return circular_mask
 
-img_width, img_height, radius = 150,150,70
+img_width, img_height, radius = 100,100,30
 mask = circular_mask(img_width, img_height, radius)
 
 im = Image.fromarray(mask)
