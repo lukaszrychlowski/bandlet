de84ff9dbff4213c68885a05d41f64f186e2df55
lukaszrychlowski <rychlowski@agh.edu.pl>
2021-10-19 23:29:36 +0200

    added transformation math

--- tests/triangular_mask.py
+++ tests/triangular_mask.py
@@ -3,12 +3,13 @@ from PIL import Image
 
 def triangular_mask(img_width, img_height, mask_angle):
     x, y = np.ogrid[:img_width, :img_height]
-    distance_field = np.abs(mask_angle * (x - int(0.5*img_width)))
-    triangular_mask = (distance_field >= (y - int(0.5*img_height))) & (~distance_field <= (y - int(0.5*img_height)))
+    distance_field = np.abs(mask_angle * (y - int(0.5*img_width)))
+    triangular_mask = (distance_field >= (x - int(0.5*img_height))) & (~distance_field <= (x - int(0.5*img_height)))
     return triangular_mask
 
-img_width, img_height, mask_angle = 150, 150, 8
+img_width, img_height, mask_angle = 9, 9, 2
 mask = triangular_mask(img_width, img_height, mask_angle)
 
 im = Image.fromarray(mask)
 im.show()
+print(mask)
